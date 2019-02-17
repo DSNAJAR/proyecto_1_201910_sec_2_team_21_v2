7590f2f28e58b1fd06477814bedcc82766a337a3
ds.najar <ds.najar@uniandes.edu.co>
2019-02-17 17:14:55 -0500

    aporte

--- src/main/MVC.java
+++ src/main/MVC.java
@@ -1,9 +1,16 @@
 package main;
 
 import controller.Controller;
-
+/**
+ * Esta es la clase pricipal del controlador
+ */
 public class MVC {
-
+	
+	/**
+	 * Ejecuta el controller
+	 * @param args Parametros de la ejecución. No son necesarios.
+	 * @throws Exception si se presenta algun error durante la ejecución.
+	 */
 	public static void main(String[] args) throws Exception {
 		Controller controler = new Controller();
 		controler.run();
